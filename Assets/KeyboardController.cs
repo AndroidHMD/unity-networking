91f8c0ad741af16e27e1551e08ceca0d8e1533fe
JK <J@c-133-37.eduroam.liu.se>
2016-01-29 14:57:22 +0100

    Add network manager and controllable cube

---
+++ Assets/KeyboardController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	const float defaultvel = 1;
+
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 velocity = Vector2.zero;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			print ("UPPP");
+
+			velocity += new Vector2 (0, defaultvel);
+
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			print ("DOWWN");
+
+			velocity -= new Vector2 (0, defaultvel);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			print ("LEFT");
+			velocity -= new Vector2 (defaultvel, 0);
+
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			print ("RIGHT");
+			velocity += new Vector2 (defaultvel, 0);
+
+		}
+
+		Rigidbody curr = gameObject.GetComponent<Rigidbody>();
+		curr.velocity = velocity;
+	}
+}
