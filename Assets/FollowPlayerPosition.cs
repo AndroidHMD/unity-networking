e2a5fb53b48a8ee29fda6f2d724eee5b324f1d4e
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-02-01 13:39:47 +0100

    Create basic FollowPlayerPosition script

---
+++ Assets/FollowPlayerPosition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.Networking;
+
+public class FollowPlayerPosition : MonoBehaviour {
+	public Vector3 relativePosition;
+
+	void Update () {
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+		foreach (GameObject player in players) {
+			NetworkIdentity networkIdentity = player.GetComponent<NetworkIdentity>();
+
+			if (networkIdentity.isLocalPlayer) {
+				Vector3 playerPosition = player.transform.position;
+				transform.position = playerPosition + relativePosition;
+
+				return;
+			}
+		}
+	}
+}
